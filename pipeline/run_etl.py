a73fd8ff004458812897f84709d49b449ac6a584
Nishanth Gandhe <nishanth.gandhe05@tamu.edu>
2025-08-18 13:28:54 -0500

    Update run_etl.py

--- pipeline/run_etl.py
+++ pipeline/run_etl.py
@@ -2,11 +2,11 @@ import os, psycopg
 from dotenv import load_dotenv
 
 load_dotenv()
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+user = os.getenv("DB_USER")
+pwd = os.getenv("DB_PASS")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db = os.getenv("DB_NAME")
 
 def run_sql_file(conn, path):
     with open(path, "r", encoding="utf-8") as f, conn.cursor() as cur:
@@ -14,7 +14,7 @@ def run_sql_file(conn, path):
 
 def main():
     print("Connecting…")
-    with psycopg.connect(user=USER, password=PASSWORD, host=HOST, port=PORT, dbname=DBNAME) as conn:
+    with psycopg.connect(user=user, password=pwd, host=host, port=port, dbname=db) as conn:
         print("Running transform.sql …")
         run_sql_file(conn, "pipeline/transform.sql")
         print("Running metrics.sql …")
